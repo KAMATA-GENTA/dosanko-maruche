cd3f768bcabcdc4a3da54c7d867a15b35d940097
Taro Tanaka <shima.tarou.2002@gmail.com>
2026-05-14 12:31:01 +0900

    entity作成

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -8,16 +8,4 @@ import lombok.Data;
 @Data
 public class Product {
 
-	private int productId; // 商品ID (PK)
-	private String productName; // 商品名
-	private String description; // 商品説明
-	private int price; // 価格
-	private int stockQuantity; // 在庫数
-	private int regionId; // 地域ID (FK)
-	private int categoryId; // カテゴリID (FK)
-	private String imageUrl; // 画像のURLやファイルパス
-	private double averageRating; // 平均評価（星の数）
-	private int salesCount; // 販売数（ランキング用）
-	private boolean isActive; // 取扱中フラグ（論理削除用）
-
 }
