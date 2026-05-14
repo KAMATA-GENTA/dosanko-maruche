5942cf127e20c16f153e07aefa6fd01c338585e6
Shinnnosuke Masuya <masu.shin20.04@gmail.com~>
2026-05-14 15:54:53 +0900

    オーダークラスのフィールド修正

--- src/main/java/com/example/demo/entity/Order.java
+++ src/main/java/com/example/demo/entity/Order.java
@@ -1,7 +1,6 @@
 package com.example.demo.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import lombok.Data;
 
@@ -13,8 +12,4 @@ public class Order {
 	private Integer subtotal;
 	private Integer shippingFee;
 	private LocalDateTime orderedAt;
-
-	// ★ 注文明細
-	private List<OrderDetail> orderDetails;
-
 }
