b1f149c170680b3fa49ffce3ba28d0b7c04bb8de
shimatarou2002-jpg <shima.tarou.2002@gmail.com>
2026-05-15 17:03:06 +0900

    lombokを使用し、@dataに修正

--- src/main/java/com/example/demo/form/LoginForm.java
+++ src/main/java/com/example/demo/form/LoginForm.java
@@ -1,23 +1,10 @@
 package com.example.demo.form;
 
+import lombok.Data;
+
+@Data
 public class LoginForm {
 	private String email;
 	private String password;
 
-	// GetterとSetter
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
