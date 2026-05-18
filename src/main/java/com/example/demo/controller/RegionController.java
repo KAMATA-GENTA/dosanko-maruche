185a3d0c94d196cad25f4c6365d5751884a949ac
kai <kaito.00231209@gmail.com>
2026-05-18 14:30:17 +0900

    地域詳細画面に背景とキャラクター画像の反映

--- src/main/java/com/example/demo/controller/RegionController.java
+++ src/main/java/com/example/demo/controller/RegionController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.Region;
@@ -14,36 +13,30 @@ public class RegionController {
 
 	// 地域詳細ページ
 	@GetMapping("/sapporo")
-	public String showSpporo(@ModelAttribute Region region, Model model) {
+	public String showSpporo(Model model) {
+		model.addAttribute("region", Region.Sapporo);
 
-		Region region1 = new Region(1, "札幌", "北海道の中心都市です");
-		model.addAttribute("region", region1);
 		return "region_test";
 	}
 
 	@GetMapping("/hakodate")
-	public String showhakodate(@ModelAttribute Region region, Model model) {
+	public String showhakodate(Model model) {
 
-		Region region2 = new Region(2, "函館", "夜景と海鮮が有名です");
-		model.addAttribute("region", region2);
+		model.addAttribute("region", Region.Hakodate);
 		return "region_test";
 	}
 
 	@GetMapping("/wakkanai")
 	public String showRegionWakkanai(Model model) {
 
-		Region region3 = new Region(3, "稚内", "北海道最北の都市です");
-		model.addAttribute("region", region3);
-		model.addAttribute("region", region3);
+		model.addAttribute("region", Region.Wakkanai);
 
 		return "region_test";
 	}
 
 	@GetMapping("/kitami")
 	public String showRegionKitami(Model model) {
-
-		Region region4 = new Region(4, "北見", "テーマパークがあります");
-		model.addAttribute("region", region4);
+		model.addAttribute("region", Region.Kitami);
 
 		return "region_test";
 	}
@@ -51,8 +44,7 @@ public class RegionController {
 	@GetMapping("/obihiro")
 	public String showRegionObihiro(Model model) {
 
-		Region region = new Region(5, "帯広", "豚丼が有名です");
-		model.addAttribute("region", region);
+		model.addAttribute("region", Region.Obihiro);
 
 		return "region_test";
 	}
