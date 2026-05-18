420f968600cebe70f21c0a07a5b7301495a4d3d7
kai <kaito.00231209@gmail.com>
2026-05-18 09:23:02 +0900

    recordの適用

--- src/main/java/com/example/demo/controller/RegionController.java
+++ src/main/java/com/example/demo/controller/RegionController.java
@@ -3,37 +3,38 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.Region;
 
 @Controller
+@RequestMapping("/region")
 public class RegionController {
 
 	// 地域詳細ページ
 	@GetMapping("/sapporo")
-	public String showSpporo(Model model) {
+	public String showSpporo(@ModelAttribute Region region, Model model) {
 
-		Region region = new Region(1);
-
-		model.addAttribute("region", region);
-
+		Region region1 = new Region(1, "札幌", "北海道の中心都市です");
+		model.addAttribute("region", region1);
 		return "region_test";
 	}
 
 	@GetMapping("/hakodate")
-	public String showHokodate(Model model) {
+	public String showhakodate(@ModelAttribute Region region, Model model) {
 
-		Region region = new Region(2);
-		model.addAttribute("region", region);
-
+		Region region2 = new Region(2, "函館", "夜景と海鮮が有名です");
+		model.addAttribute("region", region2);
 		return "region_test";
 	}
 
 	@GetMapping("/wakkanai")
 	public String showRegionWakkanai(Model model) {
 
-		Region region = new Region(3);
-		model.addAttribute("region", region);
+		Region region3 = new Region(3, "稚内", "北海道最北の都市です");
+		model.addAttribute("region", region3);
+		model.addAttribute("region", region3);
 
 		return "region_test";
 	}
@@ -41,8 +42,8 @@ public class RegionController {
 	@GetMapping("/kitami")
 	public String showRegionKitami(Model model) {
 
-		Region region = new Region(4);
-		model.addAttribute("region", region);
+		Region region4 = new Region(4, "北見", "テーマパークがあります");
+		model.addAttribute("region", region4);
 
 		return "region_test";
 	}
@@ -50,7 +51,7 @@ public class RegionController {
 	@GetMapping("/obihiro")
 	public String showRegionObihiro(Model model) {
 
-		Region region = new Region(5);
+		Region region = new Region(5, "帯広", "豚丼が有名です");
 		model.addAttribute("region", region);
 
 		return "region_test";
---
+++ src/main/java/com/example/demo/entity/Character.java
@@ -0,0 +1,5 @@
+package com.example.demo.entity;
+
+public record Character(Integer id, String name, Region region) {
+
+}
