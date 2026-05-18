ab8fcee8cf96b8cc93a87f5955f4bba4f583e9b1
優稀 <yuuki.f619@gmail.com>
2026-05-18 16:58:39 +0900

    lombokにする

--- src/main/java/com/example/demo/entity/CartItem.java
+++ src/main/java/com/example/demo/entity/CartItem.java
@@ -1,5 +1,8 @@
 package com.example.demo.entity;
 
+import lombok.Data;
+
+@Data
 public class CartItem {
 
 	private Integer id;
@@ -14,59 +17,59 @@ public class CartItem {
 		return price * quantity;
 	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Integer getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-
-	public Integer getProductId() {
-		return productId;
-	}
-
-	public void setProductId(Integer productId) {
-		this.productId = productId;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-
-	public Integer getPrice() {
-		return price;
-	}
-
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-
-	public Integer getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
+	//	public Integer getId() {
+	//		return id;
+	//	}
+	//
+	//	public void setId(Integer id) {
+	//		this.id = id;
+	//	}
+	//
+	//	public Integer getUserId() {
+	//		return userId;
+	//	}
+	//
+	//	public void setUserId(Integer userId) {
+	//		this.userId = userId;
+	//	}
+	//
+	//	public Integer getProductId() {
+	//		return productId;
+	//	}
+	//
+	//	public void setProductId(Integer productId) {
+	//		this.productId = productId;
+	//	}
+	//
+	//	public String getProductName() {
+	//		return productName;
+	//	}
+	//
+	//	public void setProductName(String productName) {
+	//		this.productName = productName;
+	//	}
+	//
+	//	public Integer getPrice() {
+	//		return price;
+	//	}
+	//
+	//	public void setPrice(Integer price) {
+	//		this.price = price;
+	//	}
+	//
+	//	public Integer getQuantity() {
+	//		return quantity;
+	//	}
+	//
+	//	public void setQuantity(Integer quantity) {
+	//		this.quantity = quantity;
+	//	}
+	//
+	//	public String getImageUrl() {
+	//		return imageUrl;
+	//	}
+	//
+	//	public void setImageUrl(String imageUrl) {
+	//		this.imageUrl = imageUrl;
+	//	}
 }
