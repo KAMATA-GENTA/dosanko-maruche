bc1268f276accc0bbd9b5caa9b3b1bfa789e7314
鎌田弦太 <kamatagenta@gizyutsukensyu.onmicrosoft.com>
2026-05-19 15:14:59 +0900

    CharactorとRegionの構造を変更しました。
    それにあたって変更のある個所を変更しました。
    
    CartItemの構造を変更しました。
    
    影響のある個所はそのままになっています。

--- src/main/java/com/example/demo/enums/Character.java
+++ src/main/java/com/example/demo/enums/Character.java
@@ -7,15 +7,16 @@ import lombok.Getter;
 @Getter
 public enum Character {
 	//(Integer id, String name, Region region)
-	SapporoChan("ノエル", "sapporonoel1.png", "sapporonoel2.png", "sapporonoel3.png"), HakodateChan("アカリ",
-			"hakodateakari1.png", "hakodateakari2.png", "hakodateakari3.png"), WakkanaiChan("しおり", "wakkanaisiori1.png",
-					"wakkanaisiori2.png", "wakkanaisiori3.png"), KitamiChan("きたみん", "kitamin1.png", "kitamin2.png",
-							"kitamin3.png"), ObihiroChan("ネユ", "obihiro1.png", "obihiro2.png",
-									"obihiro3.png"), OtaruChan("おたるん", "otarun1.png", "otarun2.png", "otarun3.png");
+	SapporoChan(1, "ノエル", "sapporonoel1.png", "sapporonoel2.png", "sapporonoel3.png"), 
+	HakodateChan(2, "アカリ","hakodateakari1.png", "hakodateakari2.png", "hakodateakari3.png"), 
+	WakkanaiChan(3, "しおり", "wakkanaisiori1.png","wakkanaisiori2.png", "wakkanaisiori3.png"),
+	KitamiChan(4, "きたみん", "kitamin1.png", "kitamin2.png","kitamin3.png"), 
+	ObihiroChan(5, "ネユ", "obihiro1.png", "obihiro2.png","obihiro3.png"),
+	OtaruChan(6, "おたるん", "otarun1.png", "otarun2.png", "otarun3.png");
 
+	private Integer id;
 	private String name;
-	private String charaImage;
-	private String charaImage2;
-	private String cahaImage3;
-
+	private String childImageUrl;
+	private String studentImageUrl;
+	private String adultImageUrl;
 }
