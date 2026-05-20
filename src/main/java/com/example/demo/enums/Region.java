627fc49662c9dac7c3fcd8d84c69092209401989
kai <kaito.00231209@gmail.com>
2026-05-20 17:33:14 +0900

    キャラクターと地域画像の調整

--- src/main/java/com/example/demo/enums/Region.java
+++ src/main/java/com/example/demo/enums/Region.java
@@ -8,12 +8,14 @@ import lombok.Getter;
 public enum Region {
 	//(Integer id, String name, String description) 
 
-	Sapporo(1, "札幌", "北海道の中心都市です", "Sapporo.png", Character.SapporoChan, "#dff5ff"), 
-	Hakodate(2, "函館", "夜景と海鮮が有名です","Hakodate.png", Character.HakodateChan,"#1b2b52"), 
-	Kitami(3, "北見", "テーマパークがあります", "Kitami.png", Character.KitamiChan, "#fff1cc"),
-	Wakkanai(4, "稚内","北海道最北の都市です", "Wakkanai.png", Character.WakkanaiChan, "#d9f2ff"), 
-	Otaru(5, "小樽","歴史的な石造りの倉庫群が残る「小樽運河」をはじめ、ガラス工芸品、オルゴール、そして新鮮な海鮮を楽しめる観光地です", "Otaru.png",Character.OtaruChan,"#d6f0ff"),
-	Obihiro(6, "帯広", "豚丼が有名です", "Obihiro.png", Character.ObihiroChan, "#ffe0cc");
+	Sapporo(1, "札幌", "北海道の中心都市です。市町村の魅力度ランキング調査でも毎年上位にランクインしていて、海鮮だけでなく、スープカレーなども有名です", "Sapporo.png",
+			Character.SapporoChan, "#dff5ff"), Hakodate(2, "函館", "夜景と海鮮が有名です", "Hakodate.png", Character.HakodateChan,
+					"#1b2b52"), Kitami(3, "北見", "テーマパークがあります", "Kitami.png", Character.KitamiChan, "#fff1cc"), Wakkanai(
+							4, "稚内", "北海道最北の都市です。タコしゃぶなどが有名で、ご当地グルメとしてチャーメンも人気です", "Wakkanai.png",
+							Character.WakkanaiChan, "#d9f2ff"), Otaru(5, "小樽",
+									"歴史的な石造りの倉庫群が残る「小樽運河」をはじめ、ガラス工芸品、オルゴール、そして新鮮な海鮮を楽しめる観光地です", "Otaru.png",
+									Character.OtaruChan, "#d6f0ff"), Obihiro(6, "帯広", "豚丼が有名です", "Obihiro.png",
+											Character.ObihiroChan, "#ffe0cc");
 
 	private final Integer id;
 	private final String name;
